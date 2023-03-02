b7031106958fe1e981dd6ad24d29af2330cbcc54
ioannis efthymiou <johnefthimiou1002@gmail.com>
2023-03-02 22:04:46 +0200

    exercise done

--- ex0.py
+++ ex0.py
@@ -1,4 +1,5 @@
 import requests  # εισαγωγή της βιβλιοθήκης
+import datetime
 
 
 def more(text):
@@ -12,8 +13,30 @@ def more(text):
                 break
 
 
-url = 'http://python.org/'  # προσδιορισμός του url
+url = 'https://www.youtube.com/'  # προσδιορισμός του url
 
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+try:
+    with requests.get(url) as response:
+        for header in response.headers:
+            print(header + response.headers[header])
+        print("\n")
+
+        server_header = response.headers.get('Server')
+        print("software:",server_header,end="\n")
+        
+
+        if response.cookies:
+            print("The web server uses cookies\n")
+            for cookie in response.cookies:
+                print("cookie name:",cookie.name)
+                if cookie.expires:
+                    print("expiration time:",datetime.datetime.fromtimestamp(cookie.expires) - datetime.datetime.now())
+                else:
+                    print("expiration time not found")
+                print("\n")
+        else:
+            print("The web server does not use cookies")
+    
+except Exception as e:
+    print("error")
+
